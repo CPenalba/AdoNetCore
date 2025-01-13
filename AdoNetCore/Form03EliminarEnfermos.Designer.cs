db4ba7524bf920d9395fff713d7b161df19a2a14
CPenalba <carolinapenalbacorpa@gmail.com>
2025-01-13 11:24:22 +0100

    Form 03

---
+++ AdoNetCore/Form03EliminarEnfermos.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace AdoNetCore
+{
+    partial class Form03EliminarEnfermos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            txtInscripcion = new TextBox();
+            btnEliminarEnfermos = new Button();
+            label2 = new Label();
+            lstEnfermos = new ListBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(48, 27);
+            label1.Name = "label1";
+            label1.Size = new Size(80, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Inscripción";
+            // 
+            // txtInscripcion
+            // 
+            txtInscripcion.Location = new Point(48, 66);
+            txtInscripcion.Name = "txtInscripcion";
+            txtInscripcion.Size = new Size(192, 27);
+            txtInscripcion.TabIndex = 1;
+            // 
+            // btnEliminarEnfermos
+            // 
+            btnEliminarEnfermos.Location = new Point(48, 124);
+            btnEliminarEnfermos.Name = "btnEliminarEnfermos";
+            btnEliminarEnfermos.Size = new Size(192, 41);
+            btnEliminarEnfermos.TabIndex = 2;
+            btnEliminarEnfermos.Text = "Eliminar enfermo";
+            btnEliminarEnfermos.UseVisualStyleBackColor = true;
+            btnEliminarEnfermos.Click += btnEliminarEnfermos_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(371, 27);
+            label2.Name = "label2";
+            label2.Size = new Size(71, 20);
+            label2.TabIndex = 3;
+            label2.Text = "Enfermos";
+            label2.Click += label2_Click;
+            // 
+            // lstEnfermos
+            // 
+            lstEnfermos.FormattingEnabled = true;
+            lstEnfermos.Location = new Point(371, 66);
+            lstEnfermos.Name = "lstEnfermos";
+            lstEnfermos.Size = new Size(290, 264);
+            lstEnfermos.TabIndex = 4;
+            // 
+            // Form03EliminarEnfermos
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lstEnfermos);
+            Controls.Add(label2);
+            Controls.Add(btnEliminarEnfermos);
+            Controls.Add(txtInscripcion);
+            Controls.Add(label1);
+            Name = "Form03EliminarEnfermos";
+            Text = "Form03EliminarEnfermos";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtInscripcion;
+        private Button btnEliminarEnfermos;
+        private Label label2;
+        private ListBox lstEnfermos;
+    }
+}
---
+++ AdoNetCore/Form03EliminarEnfermos.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
+
+namespace AdoNetCore
+{
+    public partial class Form03EliminarEnfermos : Form
+    {
+        SqlConnection cn;
+        SqlCommand com;
+        SqlDataReader reader;
+        public Form03EliminarEnfermos()
+        {
+            InitializeComponent();
+            string connectionString = @"Data Source=LOCALHOST\SQLEXPRESS;Initial Catalog=HOSPITAL;Persist Security Info=True;User ID=SA; Trust Server Certificate=True";
+            this.cn = new SqlConnection(connectionString);
+            this.com = new SqlCommand();
+            this.CargarEnfermos();
+        }
+
+        private void CargarEnfermos()
+        {
+            string sql = "select * from ENFERMO";
+            this.com.Connection = this.cn;
+            this.com.CommandType = CommandType.Text;
+            this.com.CommandText = sql;
+            this.cn.Open();
+            this.reader = this.com.ExecuteReader();
+            this.lstEnfermos.Items.Clear();
+            while (this.reader.Read())
+            {
+                string inscripcion = this.reader["INSCRIPCION"].ToString();
+                string apellido = this.reader["APELLIDO"].ToString();
+                this.lstEnfermos.Items.Add(inscripcion + " - " + apellido);
+
+            }
+            this.reader.Close();
+            this.cn.Close();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEliminarEnfermos_Click(object sender, EventArgs e)
+        {
+            string sql = "delete from ENFERMO where INSCRIPCION="
+                + this.txtInscripcion.Text;
+            this.com.Connection = this.cn;
+            this.com.CommandType = CommandType.Text;
+            this.com.CommandText = sql;
+            this.cn.Open();
+            int eliminados = this.com.ExecuteNonQuery();
+            this.cn.Close();
+            this.CargarEnfermos();
+            MessageBox.Show("Enfermos eliminados " + eliminados);
+        }
+    }
+}
